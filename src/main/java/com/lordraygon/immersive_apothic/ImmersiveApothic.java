d57ab67277f06479fe04417063a1925ab145dc4f
LordRaygon007 <barnesraymond007@gmail.com>
2026-09-14 15:32:51 +0200

    cleaning and sweeping

--- src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
+++ src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
@@ -1,10 +1,8 @@
 package com.lordraygon.immersive_apothic;
 
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
+@SuppressWarnings("unused")
 @Mod(ImmersiveApothic.MOD_ID)
 public class ImmersiveApothic {
     public static final String MOD_ID = "immersive_apothic";
