95fafede14af6a45e7f65fea7aaef3fdfb72a779
LordRaygon007 <barnesraymond007@gmail.com>
2026-09-14 15:19:28 +0200

    attempting to fix some enchantments and curses not being registered - removed config stuff as it is temperamental

--- src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
+++ src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
@@ -1,20 +1,15 @@
 package com.lordraygon.immersive_apothic;
 
-import com.lordraygon.immersive_apothic.config.Config;
-import com.lordraygon.immersive_apothic.config.EnchantmentFilter;
+
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.config.ModConfigEvent;
 
 @Mod(ImmersiveApothic.MOD_ID)
 public class ImmersiveApothic {
     public static final String MOD_ID = "immersive_apothic";
 
     public ImmersiveApothic(IEventBus modEventBus, ModContainer container) {
-        container.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
-        modEventBus.addListener(ModConfigEvent.Loading.class, EnchantmentFilter::onConfigChanged);
-        modEventBus.addListener(ModConfigEvent.Reloading.class, EnchantmentFilter::onConfigChanged);
+
     }
 }
