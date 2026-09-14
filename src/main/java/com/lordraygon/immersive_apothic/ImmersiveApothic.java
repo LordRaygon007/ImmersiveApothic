c28cd616c5da5a82082033c17f182b9e2dc7c4b2
LordRaygon007 <barnesraymond007@gmail.com>
2026-09-14 15:28:55 +0200

    removing config reg

--- src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
+++ src/main/java/com/lordraygon/immersive_apothic/ImmersiveApothic.java
@@ -8,8 +8,4 @@ import net.neoforged.fml.common.Mod;
 @Mod(ImmersiveApothic.MOD_ID)
 public class ImmersiveApothic {
     public static final String MOD_ID = "immersive_apothic";
-
-    public ImmersiveApothic(IEventBus modEventBus, ModContainer container) {
-
-    }
 }
